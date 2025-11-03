3c58b396169876daf434765a5f6dce70467b372b
Eddie Xu <eddie.xu.apple@gmail.com>
2025-11-02 23:35:51 -0500

    updated preprocessing

--- stock_data_preprocessing.py
+++ stock_data_preprocessing.py
@@ -1,6 +1,5 @@
 # This python will define the pre-processing
 
-
 # load packages
 import numpy as np
 import pandas as pd
@@ -41,3 +40,20 @@ def vif_calculation(data):
     vif_data['feature'] = feature_data.columns
     vif_data['VIF'] = [variance_inflation_factor(feature_data.values, i) for i in range(feature_data.shape[1])]
 
+    return vif_data
+
+def main():
+
+    # pull in data
+    history_data_url = 'https://raw.githubusercontent.com/eddiexunyc/capstone_work_project/refs/heads/main/Resources/historical_stock_data.csv?token=GHSAT0AAAAAADKDZNSYNLHMK3JS6HWNDL4Y2IIDJKQ'
+    history_data = pd.read_csv(history_data_url)
+
+    # perform the pre-processing
+    pre_process_data = stock_preprocessing(history_data)
+
+    # calculate the VIF
+    vif_result = vif_calculation(pre_process_data)
+    print(vif_result)
+
+if __name__=="__main__":
+    main()
